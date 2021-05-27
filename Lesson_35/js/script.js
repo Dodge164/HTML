131d6ded33706585e248b895ef8f0c45c1c0972c
Dodge164 <71329885+Dodge164@users.noreply.github.com>
2021-05-27 13:45:20 +0400

    Lesson_35

---
+++ Lesson_35/js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// let userEyesColor = green;
+
+let userName = 'Вася';
+let user = userName;
+
+console.log(user);
+// Вариант №1  вызовет ошибку
+// Вариант №2 пройдет, но записан неверно
